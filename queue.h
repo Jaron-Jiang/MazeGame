bfe423e060c13f47f1228f92114da6956a85c1c5
Jaron-Jiang <2896247746@qq.com>
2020-10-28 14:19:01 +0800

    栈的基本功能实现

---
+++ queue.h
@@ -0,0 +1,51 @@
+template<class T>
+class queue
+{
+    private:
+    struct LNode
+    {
+        T data;
+        LNode* next;
+    };
+    int length;
+    LNode* f;
+    LNode* r;
+    public:
+    queue()
+    {
+        length = 0;
+        f = r = NULL;
+    }
+    T front()
+    {
+        return f->data;
+    }
+    T back()
+    {
+        return r->data;
+    }
+    void push(T val)
+    {
+        LNode* p = new LNode;
+        p->data = val;
+        p->next = NULL;
+        length == 0 ? f = p : r->next = p;
+        r = p;
+        length++;
+    }
+    void pop()
+    {
+        LNode* p = f;
+        f = f->next;
+        delete p;
+        length--;
+    }
+    bool empty()
+    {
+        return length == 0;
+    }
+    int size()
+    {
+        return length;
+    }
+};
